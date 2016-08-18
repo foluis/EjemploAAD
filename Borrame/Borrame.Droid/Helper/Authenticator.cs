28ef562c1a0ae11c5d3599ff039882e0682fb267
Luis Forero <foluis@hotmail.com>
2016-08-18 14:15:23 -0500

    Se consume satisfactoriamente la Interfaz

--- Borrame/Borrame.Droid/Helper/Authenticator.cs
+++ Borrame/Borrame.Droid/Helper/Authenticator.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+[assembly: Dependency(typeof(Borrame.Droid.Helper.Authenticator))]
 namespace Borrame.Droid.Helper
 {
     public class Authenticator: IAuthenticator
