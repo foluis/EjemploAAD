fcc3805067641f1da8695a39bad6dc3f1c8c05d6
Luis Forero <foluis@hotmail.com>
2016-08-18 13:14:23 -0500

    Se agregó la clase que implementa la Interfaz

---
+++ Borrame/Borrame.Droid/Helper/Authenticator.cs
@@ -0,0 +1,21 @@
+using Android.App;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Borrame.Droid.Helper
+{
+    public class Authenticator: IAuthenticator
+    {
+        public async Task<AuthenticationResult> Authenticate(string authority, string resource, string clientId, string returnUri)
+        {
+            var authContext = new AuthenticationContext(authority);
+            if (authContext.TokenCache.ReadItems().Any())
+                authContext = new AuthenticationContext(authContext.TokenCache.ReadItems().First().Authority);
+            var authResult = await authContext.AcquireTokenAsync(resource, clientId, new Uri(returnUri), new PlatformParameters((Activity)Forms.Context));
+            return authResult;
+        }
+    }
+}
